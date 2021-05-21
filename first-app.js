7502c0fe32233d182221019441c1734cf3358bfe
unknown <miskoo995@gmail.com>
2021-05-21 16:15:48 +0200

    update

--- first-app.js
+++ first-app.js
@@ -1,63 +1,61 @@
-const http = require('http')
-const fs = require('fs')
+const http = require("http");
+const fs = require("fs");
 
+const data = fs.readFileSync("./data.json");
+//Parsing string.
+let projects = JSON.parse(data);
 
-const data = fs.readFileSync('./data.json')
-    //Parsing string. 
-let projects = JSON.parse(data)
-
-let lastindex = projects.length === 0 ? 1 : projects[projects.length1].id
+let lastindex = projects.length === 0 ? 1 : projects[projects.length1].id;
 
 //reading client GET
 
 const server = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,POST,PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  switch (req.method) {
+    case "GET":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(projects, null, 2));
+      console.log(projects);
+      break;
+    case "POST":
+      req.on("data", (data) => {
+        //Parsira podatke u JSON da dodje do title.
+        const jsondata = JSON.parse(data);
+        const title = jsondata.title;
+        //Provera ako title postoji.
+        if (title) {
+          // na niz projects, saljemo id, uvecan, i title.
+          projects.push({ id: ++lastindex, title: title });
+          // zapisivanje u fajl, lokacija je data.json/ m prevodi se u string, da bi mogao da sacuva taj podatak.
+          const stringtest = JSON.stringify(projects);
+          fs.writeFile("./data.json", stringtest, (err) => {
+            if (err) {
+              // U slucaju greske, prokaz poruke.
+              const message = { message: "could not persist data!" };
+              res.writeHead(500, { "Content-Type": "application/json" });
+              res.end(JSON.stringify(message, null, 2));
+            } else {
+              // u slucaju da je response 200, znaci da je zahtev prosao, izvrsava se tako sto cuva u projects.
+              res.writeHead(200, { "Content-Type": "application/json" });
+              res.end(JSON.stringify(projects, null, 2));
+            }
+          });
+        } else {
+          //Greska, title u  body ne postoji, trazi
 
-    switch (req.method) {
-        case "GET":
+          const message = { message: "no title in body request!" };
 
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(JSON.stringify(projects, null, 2))
-            console.log(projects)
-            break;
-        case "POST":
-            req.on('data', data => {
-                //Parsira podatke u JSON da dodje do title. 
-                const jsondata = JSON.parse(data);
-                const title = jsondata.title;
-                //Provera ako title postoji. 
-                if (title) {
-                    // na niz projects, saljemo id, uvecan, i title. 
-                    projects.push({ id: lastindex++, title });
-                    // zapisivanje u fajl, lokacija je data.json/ m prevodi se u string, da bi mogao da sacuva taj podatak.  
-                    fs.writeFile('./data.json', projects.toString(), (err) => {
-                        if (err) {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(message, null, 2));
+        }
+      });
 
-                            // U slucaju greske, prokaz poruke. 
-                            const message = { message: 'could not persist data!' };
-                            res.writeHead(500, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify(message, null, 2));
-                        } else {
-                            // u slucaju da je response 200, znaci da je zahtev prosao, izvrsava se tako sto cuva u projects. 
-                            res.writeHead(200, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify(projects, null, 2));
-                        }
-                    });
-                } else {
+      break;
+    default:
+      break;
+  }
+});
 
-                    //Greska, title u  body ne postoji, trazi 
-
-                    const message = { message: 'no title in body request!' };
-
-                    res.writeHead(400, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify(message, null, 2));
-                }
-            });
-
-            break;
-        default:
-            break;
-    }
-
-})
-
-server.listen(8000)
+server.listen(8000);
